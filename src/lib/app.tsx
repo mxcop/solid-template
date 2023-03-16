df5cd20b49598e1eeecd5ad7aed88de0e8c82dda
Max Coppen <44031065+mxcop@users.noreply.github.com>
2023-03-16 22:16:52 +0100

    init

---
+++ src/lib/app.tsx
@@ -0,0 +1,13 @@
+import type { Component } from 'solid-js';
+import Hello from './hello';
+
+const App: Component = () => {
+  return (
+    <>
+      <h1>Hello world!!!!</h1>
+      <Hello />
+    </>
+  );
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+
+export default defineConfig({
+  plugins: [solidPlugin()],
+  server: {
+    port: 5258,
+    strictPort: true,
+  },
+  build: {
+    outDir: '.solid',
+    minify: 'terser',
+    target: 'esnext',
+  },
+  logLevel: 'info'
+});
